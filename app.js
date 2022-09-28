3f2c5746f35f210f4017a9cbe29548b6a17c5fe5
Camilo <camilo.velezp@udea.edu.co>
2022-09-28 13:30:25 -0500

    cambio ruta base

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.use("/api", require("./Routes/UserRoutes"))
+app.use("/ingemon", require("./Routes/UserRoutes"))
 app.use("/ingemon", require("./Routes/IngemonRoutes"))
 
 //revisar conexion
